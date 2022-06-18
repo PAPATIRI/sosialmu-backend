3df22a906505c71e76cb42aa678c88f6c7d1f599
syarif taufik <syariftaufik11@gmail.com>
2022-06-18 21:36:43 +0700

    finished delete a post and like dislike a post features

--- routes/posts.js
+++ routes/posts.js
@@ -27,8 +27,38 @@ router.put("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
 //DELETE A POST
-//LIKE A POST
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.userId === req.body.userId) {
+      await post.deleteOne({ $set: req.body });
+      res.status(200).json("the post has been deleted");
+    } else {
+      res.status(403).json("you can delete only your post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LIKE DISLIKE A POST
+router.put("/:id/like", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+      res.status(200).json("you liked this post");
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+      res.status(200).json("you disliked this post");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //GET A POST
 //GET TIMELINE POST
 
